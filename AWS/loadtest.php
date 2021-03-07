f81285695c845d9188a8455020bbd8af4c013888
egoing <egoing@gmail.com>
2021-03-07 11:36:37 +0900

    Update loadtest.php

--- AWS/loadtest.php
+++ AWS/loadtest.php
@@ -1,5 +1,5 @@
 <?php
 $time = microtime(true);
-password_hash('Hello',CRYPT_BLOWFISH,array("cost"=>14));
+password_hash('Hello',CRYPT_BLOWFISH,array("cost"=>empty($_GET['cost']) ? 14 : (int)$_GET['cost']));
 echo microtime(true) - $time;
 ?>
