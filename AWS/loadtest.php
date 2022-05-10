9cc7093ebccee3de23ed736d2adbc2ebf7ce0f5e
egoing <egoing@gmail.com>
2022-05-10 13:20:58 +0900

    Update loadtest.php

--- AWS/loadtest.php
+++ AWS/loadtest.php
@@ -1,5 +1,8 @@
 <?php
 $time = microtime(true);
+date_default_timezone_set("Asia/Seoul");
 password_hash('Hello',CRYPT_BLOWFISH,array("cost"=>empty($_GET['cost']) ? 14 : (int)$_GET['cost']));
-echo $_SERVER['SERVER_ADDR'].' : '.(microtime(true) - $time);
+echo 'IP address : '. $_SERVER['SERVER_ADDR'].'<br>';
+echo '현재 시간 : '.date("Y-m-d h:i:sa").'<br>';
+echo '소요 시간 : '.(microtime(true) - $time).'<br>';
 ?>
